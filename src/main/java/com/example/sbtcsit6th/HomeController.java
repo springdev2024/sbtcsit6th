bd6641d333a999770806f742a49596534a4a8727
springdev2024 <springdev2024@gmail.com>
2024-11-24 21:18:40 +0545

    Removed redundant /time handler

--- src/main/java/com/example/sbtcsit6th/HomeController.java
+++ src/main/java/com/example/sbtcsit6th/HomeController.java
@@ -1,7 +1,5 @@
 package com.example.sbtcsit6th;
 
-import java.util.Date;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +9,6 @@ public class HomeController {
 	@GetMapping("/home")
 	public String home() {
 		return "Hello World";
-	}
-
-	@GetMapping("/time")
-	public String time() {
-		return "Current time is " + (new Date());
 	}
 
 }
--- src/main/java/com/example/sbtcsit6th/TodoController.java
+++ src/main/java/com/example/sbtcsit6th/TodoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TodoController {
 
-	@GetMapping
+	@GetMapping("/todo")
 	public String getTodo() {
 		return "todo";
 	}
